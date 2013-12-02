37b57d7f3b7a61af50c00870c053383425c7ba98
Thomas Poulet <dark.flames0@gmail.com>
2013-12-02 09:34:11 +0100

    First attempt
    Based on mirrored calendar file

---
+++ example.js
@@ -0,0 +1,182 @@
+var JSONagenda = [
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-27T14:00:00",
+      "end":"2013-11-27T15:30:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-28T11:30:00",
+      "end":"2013-11-28T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-02T09:45:00",
+      "end":"2013-12-02T11:15:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-06T13:30:00",
+      "end":"2013-12-06T15:00:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-11T11:30:00",
+      "end":"2013-12-11T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118/1314S5 - Théorie des graphes - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-09T09:45:00",
+      "end":"2013-12-09T11:15:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117/1314S5 - Langages et compilation - KASSEL, Helen - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-27T11:30:00",
+      "end":"2013-11-27T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117/1314S5 - Langages et compilation - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-11-25T17:00:00",
+      "end":"2013-11-25T18:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117/1314S5 - Langages et compilation - KASSEL, Helen - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-12-04T14:00:00",
+      "end":"2013-12-04T15:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117/1314S5 - Langages et compilation - KASSEL, Helen - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-26T09:45:00",
+      "end":"2013-11-26T11:15:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117/1314S5 - Langages et compilation - KASSEL, Helen - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-09T11:30:00",
+      "end":"2013-12-09T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-06T15:15:00",
+      "end":"2013-12-06T16:45:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-26T11:30:00",
+      "end":"2013-11-26T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,A,GRANDAMPHI",
+      "start":"2013-11-25T11:00:00",
+      "end":"2013-11-25T12:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-11-27T15:45:00",
+      "end":"2013-11-27T17:15:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-02T08:00:00",
+      "end":"2013-12-02T09:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TI122/1314S5 - Programmation Java 1 - CHARROUX, Benoit - VILLEJUIF,A,GRANDAMPHI",
+      "start":"2013-12-13T11:30:00",
+      "end":"2013-12-13T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TE107/1314S5 - Communications numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-06T11:00:00",
+      "end":"2013-12-06T12:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TE107/1314S5 - Communications numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-13T14:00:00",
+      "end":"2013-12-13T15:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TE107/1314S5 - Communications numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-13T15:45:00",
+      "end":"2013-12-13T17:15:00",
+      "allDay":false
+   },
+   {
+      "title":"TE107/1314S5 - Communications numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-06T09:15:00",
+      "end":"2013-12-06T10:45:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-11-29T09:15:00",
+      "end":"2013-11-29T10:45:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-11-29T11:00:00",
+      "end":"2013-11-29T12:30:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-12-02T11:30:00",
+      "end":"2013-12-02T13:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-11-25T13:30:00",
+      "end":"2013-11-25T15:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-12-05T09:45:00",
+      "end":"2013-12-05T11:15:00",
+      "allDay":false
+   },
+   {
+      "title":"TE108/1314S5 - Codages numériques - OSSONCE, Maxime - VILLEJUIF,F,AMPHIORANGE",
+      "start":"2013-11-25T15:15:00",
+      "end":"2013-11-25T16:45:00",
+      "allDay":false
+   },
+   {
+      "title":"SM118-CE/1314S5 - Théorie des graphes - DOE, John - VILLEJUIF,F,AMPHIROUGE; VILLEJUIF,A,GRANDAMPHI",
+      "start":"2013-12-11T09:00:00",
+      "end":"2013-12-11T10:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117-CE/1314S5 - Langages et compilation - DOE, John - VILLEJUIF,F,AMPHIROUGE",
+      "start":"2013-12-11T10:00:00",
+      "end":"2013-12-11T11:00:00",
+      "allDay":false
+   },
+   {
+      "title":"TI117-CE/1314S5 - Langages et compilation - DOE, John - VILLEJUIF,A,GRANDAMPHI",
+      "start":"2013-12-11T10:00:00",
+      "end":"2013-12-11T11:00:00",
+      "allDay":false
+   }
+];
